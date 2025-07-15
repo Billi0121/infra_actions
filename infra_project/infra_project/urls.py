cd8e4b52b0257653e269d3f60e7a8ff240dd1259
Billi0121 <gafarovbilol2009@gmail.com>
2025-07-15 12:16:35 +0500

    Flake8

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from infra_app import views
 
 urlpatterns = [
-    path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
+    path('', views.index),
 ]
