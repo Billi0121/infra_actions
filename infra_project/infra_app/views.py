2963dda677cde0b006e6486a13f4204562cc2412
Billi0121 <gafarovbilol2009@gmail.com>
2025-07-22 11:53:31 +0500

    POka

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,10 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("О да ")
+    return HttpResponse("Little Think")
 
 
 def second_page(request):
     return HttpResponse("А это вторая страница WWW")
-
-#dddD
